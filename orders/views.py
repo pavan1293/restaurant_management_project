c637fc2a1b974eb162d498ee6017e1a6c50611d8
pavan1293 <sdvpavan123@gmail.com>
2025-08-06 21:43:59 +0530

    18974

--- orders/views.py
+++ orders/views.py
@@ -2,3 +2,23 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'restaurant/home.html')
+    
+    def get_menu(request):
+    menu = [
+        {
+            "name": "Paneer Butter Masala",
+            "description": "Creamy tomato curry with soft paneer cubes.",
+            "price": 220
+        },
+        {
+            "name": "Veg Biryani",
+            "description": "Aromatic basmati rice with vegetables and spices.",
+            "price": 180
+        },
+        {
+            "name": "Masala Dosa",
+            "description": "Crispy dosa stuffed with spicy potato filling.",
+            "price": 90
+        },
+    ]
+    return Response(menu)
