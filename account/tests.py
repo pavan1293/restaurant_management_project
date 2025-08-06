c458fbd3137eaa06c7f216cc0b528fb4d2fbba22
pavan1293 <sdvpavan123@gmail.com>
2025-08-06 21:12:44 +0530

    18970

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import UserProfile
 
-# Create your tests here.
+class UserProfileTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpass')
+        self.user.profile.full_name = 'Test User'
+        self.user.profile.phone_number = '+911234567890'
+        self.user.profile.save()
+
+    def test_profile_created(self):
+        """Test that the user profile is created automatically."""
+        profile = self.user.profile
+        self.assertEqual(profile.full_name, 'Test User')
+        self.assertEqual(profile.phone_number, '+911234567890')
+        self.assertEqual(profile.user.username, 'testuser')
