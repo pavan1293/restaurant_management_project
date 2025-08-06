59dbdaeb2ade60a25fd5e589705bc42796159e56
pavan1293 <sdvpavan123@gmail.com>
2025-08-06 21:14:21 +0530

    18970

--- account/views.py
+++ account/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def profile_view(request):
+    return render(request, 'users/profile.html', {
+        'user': request.user,
+        'profile': request.user.profile
+    })
