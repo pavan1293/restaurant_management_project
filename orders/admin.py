a0bd1bf665f2f3c7a068a92758ee7ac216972b10
pavan1293 <sdvpavan123@gmail.com>
2025-08-06 21:20:16 +0530

    18971

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,9 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import MenuItem
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price')     
+    search_fields = ('name',)            
+    list_filter = ('price',)            
