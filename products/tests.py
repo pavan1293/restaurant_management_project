9312f1b42c3303d84076ba7962b021037f3d10dc
pavan1293 <sdvpavan123@gmail.com>
2025-08-06 21:24:36 +0530

    18971

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
 
-# Create your tests here.
+from django.test import TestCase
+from .models import MenuItem
+
+class MenuItemModelTest(TestCase):
+    def setUp(self):
+        self.item = MenuItem.objects.create(
+            name="Veg Biryani",
+            price=180.00,
+            description="Aromatic rice with spices and vegetables"
+        )
+
+    def test_menu_item_content(self):
+        self.assertEqual(self.item.name, "Veg Biryani")
+        self.assertEqual(float(self.item.price), 180.00)
+        self.assertEqual(self.item.description, "Aromatic rice with spices and vegetables")
+        self.assertEqual(str(self.item), "Veg Biryani")
