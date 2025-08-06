4e32a30df7df99ffe2a52e53a080d130128c9226
pavan1293 <sdvpavan123@gmail.com>
2025-08-06 21:39:53 +0530

    18973

--- orders/admin.py
+++ orders/admin.py
@@ -1,4 +1,3 @@
-
 from django.contrib import admin
 from .models import MenuItem
 
@@ -6,4 +5,18 @@ from .models import MenuItem
 class MenuItemAdmin(admin.ModelAdmin):
     list_display = ('name', 'price')     
     search_fields = ('name',)            
-    list_filter = ('price',)            
+    list_filter = ('price',)      
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('menu_item', 'quantity')
+    list_filter = ('menu_item',)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'customer', 'total_amount', 'status', 'created_at')
+    list_filter = ('status', 'created_at')
+    search_fields = ('customer__username',)
+    filter_horizontal = ('items',)  # Better interface for ManyToManyField
+    date_hierarchy = 'created_at'      
