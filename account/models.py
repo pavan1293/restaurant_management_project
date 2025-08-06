550fde4ae5c3d50335b5e522c6b8f4af3d4d802c
pavan1293 <sdvpavan123@gmail.com>
2025-08-06 21:10:27 +0530

    18970

--- account/models.py
+++ account/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    full_name = models.CharField(max_length=100)
+    phone_number = models.CharField(
+        max_length=15,
+        validators=[RegexValidator(r'^\+?1?\d{9,15}$')],
+        help_text="Enter a valid phone number. E.g., +919876543210"
+    )
+
+    def __str__(self):
+        return self.user.username
