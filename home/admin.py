b40fd7d6a07fd1e4f2ecfda96f8ea64170263cb0
pavan1293 <sdvpavan123@gmail.com>
2025-11-30 19:12:05 +0530

    29975

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Driver
 
-# Register your models here.
+class DriverAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "latitude", "longitude", "is_available")
+    list_filter = ("is_available",)
+    search_fields = ("name",)
