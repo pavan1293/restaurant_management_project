6429443fd4ad64c5fbedeace2c916abbeeb1af10
pavan1293 <sdvpavan123@gmail.com>
2025-11-30 19:12:51 +0530

    29975

--- home/models.py
+++ home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Driver(models.Model):
+    name = models.CharField(max_length=100)
+    latitude = models.FloatField(null=True, blank=True)
+    longitude = models.FloatField(null=True, blank=True)
+    is_available = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name} (ID: {self.id})"
